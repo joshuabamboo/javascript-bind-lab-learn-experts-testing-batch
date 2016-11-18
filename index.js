f4e9bfea9099c138ce3aaeb349ed9ba2730943ab
jprosario <joshuaowens011@gmail.com>
2016-11-18 18:49:03 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round((Date.now())/1000),
+  startTicking: function () {
+    setInterval(()=>this.time = this.time + 1, 1000);
+  }
+}
+
+const censor = (word,sentence) => {
+  let regex = new RegExp(word, 'g');
+  return sentence.replace(regex, 'BLEEP');
+}
+
+const drugsCensor = censor.bind(null, 'drugs');
+const violenceCensor = censor.bind(null, 'violence');
